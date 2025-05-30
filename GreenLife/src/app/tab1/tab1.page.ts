57bf83bd5a17411dcf55201274280ea90dab4cda
CHidalgo405 <carlosignacio.9836@gmail.com>
2025-05-29 22:08:37 -0600

    Se actualizo tab1

--- GreenLife/src/app/tab1/tab1.page.ts
+++ GreenLife/src/app/tab1/tab1.page.ts
@@ -9,11 +9,10 @@ import { Component } from '@angular/core';
 
 export class Tab1Page{
   productos = [
-    { nombre: 'Lechuga tipo 1', descripcion: 'No se que poner pero aca va la descripción.', precio: 25 },
-    { nombre: 'Lechuga tipo 2', descripcion: 'No se que poner pero aca va la descripción.', precio: 40 },
-    { nombre: 'Lechuga tipo 3', descripcion: 'No se que poner pero aca va la descripción.', precio: 35 },
-    { nombre: 'Lechuga tipo 4', descripcion: 'No se que poner pero aca va la descripción.', precio: 30 },
-    { nombre: 'Lechuga tipo 5', descripcion: 'No se que poner pero aca va la descripción.', precio: 50 },
+    { nombre: 'Lechuga', descripcion: 'No se que poner pero aca va la descripción.', precio: 25 },
+    { nombre: 'Lechuga', descripcion: 'No se que poner pero aca va la descripción.', precio: 40 },
+    { nombre: 'Lechuga', descripcion: 'No se que poner pero aca va la descripción.', precio: 35 },
+    { nombre: 'Lechuga', descripcion: 'No se que poner pero aca va la descripción.', precio: 30 },
     // Agrega más productos si quieres
   ];
 
